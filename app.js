e72b5d72216886672ad8158ab81908664c909210
Alitedigitals2007 <161524740+Alitedigitals2007@users.noreply.github.com>
2026-04-04 05:35:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use(passport.session());
 // 2. Google Strategy
 passport.use(new GoogleStrategy({
     clientID: "386993811253-3g3cjkq8nkp43f5fns33m8pclk1cifq3.apps.googleusercontent.com",
-    clientSecret: "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",
+    clientSecret: "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",
     callbackURL: "/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
